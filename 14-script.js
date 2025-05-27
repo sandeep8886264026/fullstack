831132ce376d7020f4c0ce1977c18a53f59afbd8
sandeep <sandeepmg.fullstack@gmail.com>
2025-05-27 07:02:48 +0530

    sandeep_888005

--- 14-script.js
+++ 14-script.js
@@ -1,36 +1,35 @@
-//var and let , const keywords - variables 
-// Target i want to prepare the local variables in the local block
-//Ans use let keyword
-var greenColor = "green";
+// var and let, const keywords - variables
+// Target: Use let keyword to prepare local variables in a local block
 
-  // console.log("%c")s
+var greenColor = "green"; // Global variable
 
-  //Local block1
+// Local Block 1
 {
+  console.log("LocalBlock-1 - greenColor:", greenColor); // green
 
-  console.log("LocalBlock-1", greenColor); // green
-  let blueColor = "blue";
-  console.log("LocalBlock-1", bluecolor); // blue
-
+  let blueColor = "blue"; // Local variable
+  console.log("LocalBlock-1 - blueColor:", blueColor); // blue
 }
- //Local Block 2 
- 
+
+// Local Block 2
 {
-  console.log("LocalBlock-1", greenColor); // green
-  let blueColor = "blue";
-  console.log("LocalBlock-1", bluecolor); // blue
+  console.log("LocalBlock-2 - greenColor:", greenColor); // green
 
+  let blueColor = "blue"; // Local variable (separate from Block 1)
+  console.log("LocalBlock-2 - blueColor:", blueColor); // blue
 }
- 
- //Example 2
 
- var signalName = "wi-fi";
+// Example 2
+var signalName = "wi-fi"; // Global variable
 
 {
-   //Let say room1
-   console.log("signal :", signalName); // wifi
-   
-   let room1FanSpeed = "high";
-   Console.log("FanSpeed :" , Room1FanSpeed); // high
-   //console.log(FanSpeed :" , room2FanSpeed); // Reference Error : room 2
+  // Room 1 block
+  console.log("Room1 - signalName:", signalName); // wi-fi
+
+  let room1FanSpeed = "high"; // Local variable
+  console.log("Room1 - FanSpeed:", room1FanSpeed); // high
 }
+
+  // net the following line would throw ReferenceError
+  // because room2FanSpeed is not defined in this block
+  // console.log("Room1 - room2FanSpeed:", room2Fan
